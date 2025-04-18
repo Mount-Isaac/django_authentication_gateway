f36acd97ee074b85cf335c7f54f7c0df4a68bbde
isaac kyalo <kyalomutie@students.ku.ac.ke>
2025-04-18 23:23:17 +0300

    user registration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import (
+    CustomUser
+)
+
 # Register your models here.
+def register_models(*models):
+    [admin.site.register(model) for model in models]
+
+
+register_models(
+    CustomUser
+)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 # Create your views here.
+
+class RegisterUserView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = request.data
+            user_serializer = UserSerializer(data=data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response({"message": "user registered successfully", "user":user_serializer.data}, status=status.HTTP_201_CREATED)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # yet to log 
+            return Response(status=status.HTTP_400_BAD_REQUEST)
