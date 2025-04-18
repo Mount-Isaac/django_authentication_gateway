977d8abb91696dec3ebb3c8717b1733bc47e8cb5
isaac kyalo <kyalomutie@students.ku.ac.ke>
2025-04-18 23:42:51 +0300

    reset password

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import register_user
+from .views import (
+    register_user,
+    reset_password
+)
 
 urlpatterns = [
-    path("register/", register_user, name='register-user')
+    path("register/", register_user, name='register-user'),
+    path("reset/password/", reset_password, name='reset-password')
 ]
